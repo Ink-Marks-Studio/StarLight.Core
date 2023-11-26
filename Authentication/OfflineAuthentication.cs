871d9babb18145602eeecd524f97da203daa63cb
zhi-feng2008 <z1258909489@outlook.com>
2023-11-26 23:29:59 +0800

    添加离线验证器
    添加离线验证器

--- Authentication/OfflineAuthentication.cs
+++ Authentication/OfflineAuthentication.cs
@@ -1,7 +1,50 @@
-﻿namespace StarLight.Core.Authentication
+﻿using System.Text.RegularExpressions;
+using StarLight.Core.Enum;
+
+namespace StarLight.Core.Authentication
 {
     public static class OfflineAuthentication
     {
+        public static string AccessToken { get; set; }
+        public static string ClientToken { get; set; }
+        public static string Name { get; set; }
+        public static string Uuid { get; set; }
+        public static async Task<Dictionary<string, string>> Authenticate(string username,string uuid = default)
+        {
+            AccessToken = Guid.NewGuid().ToString("N");
+            ClientToken = Guid.NewGuid().ToString("N");
+            Name = username;
+            
+            if (uuid == default) {
+                Uuid = Guid.NewGuid().ToString();
+            }
+            else if (IsValidUuid(uuid)) {
+                Uuid = uuid;
+            }
+            else
+            {
+                Uuid = Guid.NewGuid().ToString();
+            }
+            
+            Dictionary<string, string> result = new Dictionary<string, string>
+            {
+                { "AuthType", AuthType.Offline.ToString() },
+                { "accessToken", AccessToken },
+                { "clientToken", ClientToken },
+                { "id", Uuid },
+                { "name", username }
+            };
+
+            return result;
+        }
         
+        // 验证Uuid是否合法
+        public static bool IsValidUuid(string uuid)
+        {
+            string pattern = @"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
+            Regex regex = new Regex(pattern);
+
+            return regex.IsMatch(uuid);
+        }
     }
 }
