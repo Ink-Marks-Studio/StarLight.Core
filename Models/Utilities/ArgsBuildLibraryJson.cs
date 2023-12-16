91786e1e9a5eda80f8d44c142882a4da1d587cf1
zhi-feng2008 <z1258909489@outlook.com>
2023-12-16 20:37:25 +0800

    完成非继承类的CP参数构建
    完成非继承类的CP参数构建

--- Models/Utilities/ArgsBuildLibraryJson.cs
+++ Models/Utilities/ArgsBuildLibraryJson.cs
@@ -1,6 +1,42 @@
+using System.Text.Json.Serialization;
+
 namespace StarLight_Core.Models.Utilities;
 
 public class ArgsBuildLibraryJson
 {
+    [JsonPropertyName("libraries")]
+    public List<Library> Libraries { get; set; }
+}
+
+public class Library
+{
+    [JsonPropertyName("name")]
     public string Name { get; set; }
+    
+    [JsonPropertyName("downloads")]
+    public Download Downloads { get; set; }
+}
+
+public class Download
+{
+    [JsonPropertyName("artifact")] 
+    public Artifact Artifact { get; set; }
+
+    [JsonPropertyName("classifiers")] 
+    public Dictionary<string, Artifact> Classifiers = null;
+}
+
+public class Artifact
+{
+    [JsonPropertyName("path")]
+    public string Path { get; set; }
+
+    [JsonPropertyName("sha1")]
+    public string Sha1 { get; set; }
+
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
 }
--- Utilities/ArgumentsBuildUtil.cs
+++ Utilities/ArgumentsBuildUtil.cs
@@ -5,20 +5,55 @@ namespace StarLight_Core.Utilities;
 
 public class ArgumentsBuildUtil
 {
-    public string BuildLibrariesArgs(string versionId, string root)
+    public static string BuildLibrariesArgs(string versionId, string root)
     {
         GameCoreInfo coreInfo = GameCoreUtil.GetGameCore(versionId, root);
-        string versionPath = root + "\\" + versionId + versionId + ".json";
+        string versionPath = coreInfo.root + "\\" + versionId + ".json";
+        Console.WriteLine(versionPath);
+        string librariesPath = "null";
+        if (FileUtil.IsAbsolutePath(root))
+        {
+            librariesPath = root + "\\libraries";
+        }
+        else
+        {
+            librariesPath = FileUtil.GetCurrentExecutingDirectory() + "\\" + root + "\\libraries";
+        }
+        
         if (coreInfo.InheritsFrom != null!)
         {
             
         }
         else
         {
-            var libraries = JsonSerializer.Deserialize<List<ArgsBuildLibraryJson>>(versionPath);
-            
+            var jsonData = File.ReadAllText(versionPath);
+            var argsLibraries = JsonSerializer.Deserialize<ArgsBuildLibraryJson>(jsonData);
+            var cps = new List<string>();
+
+            foreach (var lib in argsLibraries.Libraries)
+            {
+                if (lib.Downloads.Classifiers?.Count == 0 || lib.Downloads.Classifiers == null)
+                {
+                    var path = BuildUrlFromName(lib.Name, librariesPath);
+                    if (!path.Contains("ca"))
+                    {
+                        cps.Add(path);
+                    }
+                }
+            }
+
+            return string.Join(";", cps);
         }
 
         return "";
+    }
+    
+    // 构建 Libraries 路径
+    private static string BuildUrlFromName(string name, string root)
+    {
+        var parts = name.Split(':');
+        if (parts.Length != 3) throw new ArgumentException("[SLC]名称格式无效,获取错误");
+        
+        return $"{root}\\{parts[0].Replace('.', '\\')}\\{parts[1]}\\{parts[2]}\\{parts[1]}-{parts[2]}.jar";
     }
 }
