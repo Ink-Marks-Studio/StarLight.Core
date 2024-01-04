58be999aa0ab93220aeffeccd65450a79af8c8a9
zhi-feng2008 <z1258909489@outlook.com>
2024-01-04 23:21:09 +0800

    优化下构建
    优化下构建

--- Utilities/ArgumentsBuildUtil.cs
+++ Utilities/ArgumentsBuildUtil.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using StarLight_Core.Models.Utilities;
 using System.Text.Json;
 using StarLight_Core.Models.Authentication;
@@ -29,81 +30,69 @@ public class ArgumentsBuildUtil
         VersionId = gameCoreConfig.Version;
         Root = gameCoreConfig.Root;
     }
+
+    public List<string> Build()
+    {
+        List<string> arguments = new List<string>();
+        arguments.Add(BuildLibrariesArgs());
+        return arguments;
+    }
     
     public string BuildLibrariesArgs()
     {
-        GameCoreInfo coreInfo = GameCoreUtil.GetGameCore(VersionId, Root);
-        string versionPath = coreInfo.root + "\\" + VersionId + ".json";
-        string librariesPath = "null";
-        string InheritFromPath = "null";
-        if (FileUtil.IsAbsolutePath(Root))
+        try
         {
-            librariesPath = Root + "\\libraries";
-            InheritFromPath = Root + "\\versions\\" + coreInfo.InheritsFrom + "\\" + coreInfo.InheritsFrom + ".json";
-        }
-        else
-        {
-            librariesPath = FileUtil.GetCurrentExecutingDirectory() + "\\" + Root + "\\libraries";
-            InheritFromPath = FileUtil.GetCurrentExecutingDirectory() + "\\" + Root + "\\versions\\" + coreInfo.InheritsFrom + "\\" + coreInfo.InheritsFrom + ".json";
-        }
-        
-        if (coreInfo.InheritsFrom != null)
-        {
-            var fromJsonData = File.ReadAllText(InheritFromPath);
-            var fromArgsLibraries = JsonSerializer.Deserialize<ArgsBuildLibraryJson>(fromJsonData);
-            var cps = new List<string>();
-            
-            foreach (var lib in fromArgsLibraries.Libraries)
-            {
-                if (lib.Downloads.Classifiers == null || lib.Downloads.Classifiers.Count == 0)
-                {
-                    var path = BuildUrlFromName(lib.Name, librariesPath);
-                    if (!path.Contains("ca"))
-                    {
-                        cps.Add(path);
-                    }
-                }
-            }
-            
-            var jsonData = File.ReadAllText(versionPath);
-            var argsLibraries = JsonSerializer.Deserialize<ArgsBuildLibraryJson>(jsonData);
-            foreach (var lib in argsLibraries.Libraries)
-            {
-                var path = BuildUrlFromName(lib.Name, librariesPath);
-                cps.Add(path);
-            }
-            return string.Join(";", cps);
-        }
-        else
-        {
-            var jsonData = File.ReadAllText(versionPath);
-            var argsLibraries = JsonSerializer.Deserialize<ArgsBuildLibraryJson>(jsonData);
+            GameCoreInfo coreInfo = GameCoreUtil.GetGameCore(VersionId, Root);
+            string versionPath = Path.Combine(coreInfo.root, $"{VersionId}.json");
+            string librariesPath = FileUtil.IsAbsolutePath(Root) ? 
+                Path.Combine(Root, "libraries") : 
+                Path.Combine(FileUtil.GetCurrentExecutingDirectory(), Root, "libraries");
+
             var cps = new List<string>();
 
-            foreach (var lib in argsLibraries.Libraries)
+            string InheritFromPath = coreInfo.InheritsFrom != null ?
+                Path.Combine(Root, "versions", coreInfo.InheritsFrom, $"{coreInfo.InheritsFrom}.json") : 
+                null;
+
+            if (InheritFromPath != null)
             {
-                if (lib.Downloads.Classifiers == null || lib.Downloads.Classifiers.Count == 0)
-                {
-                    var path = BuildUrlFromName(lib.Name, librariesPath);
-                    if (!path.Contains("ca"))
-                    {
-                        cps.Add(path);
-                    }
-                }
+                cps.AddRange(ProcessLibraryPath(InheritFromPath, librariesPath));
             }
+
+            cps.AddRange(ProcessLibraryPath(versionPath, librariesPath));
 
             return string.Join(";", cps);
         }
-
-        return "";
+        catch (Exception ex)
+        {
+            throw new Exception($"[SL]构建Library参数错误: + {ex.Message}");
+        }
     }
+
+    private IEnumerable<string> ProcessLibraryPath(string filePath, string librariesPath)
+    {
+        var jsonData = File.ReadAllText(filePath);
+        var argsLibraries = JsonSerializer.Deserialize<ArgsBuildLibraryJson>(jsonData);
+
+        foreach (var lib in argsLibraries.Libraries)
+        {
+            if (lib.Downloads.Classifiers == null || lib.Downloads.Classifiers.Count == 0)
+            {
+                var path = BuildFromName(lib.Name, librariesPath);
+                if (!path.Contains("ca"))
+                {
+                    yield return path;
+                }
+            }
+        }
+    }
+
     
-    // 构建 Libraries 路径
-    private string BuildUrlFromName(string name, string root)
+    private string BuildFromName(string name, string root)
     {
         var parts = name.Split(':');
-        if (parts.Length != 3) throw new ArgumentException("[SLC]名称格式无效,获取错误");
-        
-        return $"{root}\\{parts[0].Replace('.', '\\')}\\{parts[1]}\\{parts[2]}\\{parts[1]}-{parts[2]}.jar";
+        if (parts.Length != 3) throw new ArgumentException("[SL]名称格式无效,获取错误");
+
+        return Path.Combine(root, parts[0].Replace('.', '\\'), parts[1], parts[2], $"{parts[1]}-{parts[2]}.jar");
     }
 }
