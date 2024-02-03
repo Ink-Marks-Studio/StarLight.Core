83a311691a208fed25af0426a3f55e6ccce15887
zhi-feng2008 <z1258909489@outlook.com>
2024-02-03 23:41:10 +0800

    整理代码
    整理代码

--- Installer/MinecraftInstaller.cs
+++ Installer/MinecraftInstaller.cs
@@ -1,102 +1,12 @@
-﻿using System.Text.Json;
-using StarLight_Core.Models.Installer;
-using StarLight_Core.Utilities;
-
-namespace StarLight_Core.Installer
+﻿namespace StarLight_Core.Installer
 {
     public class MinecraftInstaller
     {
-        // 获取指定版本
-        public static async Task<GameCoreDownloadInfo> GetGameCoreAsync(string id)
-        {
-            var gameCoresInfo = await GetGameCoresAsync().ConfigureAwait(false);
-            var gameCoreInfo = gameCoresInfo.FirstOrDefault(x => x.Id == id);
-            
-            if (gameCoreInfo == null)
-            {
-                throw new InvalidOperationException("[SL]版本不存在");
-            }
-            
-            return gameCoreInfo;
-        }
+        public string GameId { get; set; }
         
-        // 获取版本列表
-        public static async Task<IEnumerable<GameCoreDownloadInfo>> GetGameCoresAsync()
+        public MinecraftInstaller(string gameId)
         {
-            try
-            {
-                var json = await HttpUtil.GetJsonAsync("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json").ConfigureAwait(false);
-                if (string.IsNullOrWhiteSpace(json))
-                {
-                    throw new InvalidOperationException("[SL]版本列表为空");
-                }
-
-                var versionsManifest = JsonSerializer.Deserialize<GameCoreJsonEntity>(json);
-                if (versionsManifest?.Version == null)
-                {
-                    throw new InvalidOperationException("[SL]版本列表为空");
-                }
-
-                return versionsManifest.Version.Select(x => new GameCoreDownloadInfo
-                {
-                    Id = x.Id,
-                    Type = x.Type,
-                    Url = x.Url,
-                    Time = x.Time,
-                    ReleaseTime = x.ReleaseTime,
-                    Sha1 = x.Sha1
-                });
-            }
-            catch (JsonException je)
-            {
-                throw new Exception("[SL]版本列表解析失败：" + je.Message, je);
-            }
-            catch (HttpRequestException hre)
-            {
-                throw new Exception("[SL]下载版本列表失败：" + hre.Message, hre);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("[SL]获取版本列表时发生未知错误：" + e.Message, e);
-            }
-        }
-        
-        // 获取最新版本
-        public static async Task<LatestGameCoreDownloadInfo> GetLatestGameCoreAsync()
-        {
-            try
-            {
-                var json = await HttpUtil.GetJsonAsync("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json").ConfigureAwait(false);
-                if (string.IsNullOrWhiteSpace(json))
-                {
-                    throw new InvalidOperationException("[SL]版本列表为空");
-                }
-
-                var versionsManifest = JsonSerializer.Deserialize<GameCoreJsonEntity>(json);
-                if (versionsManifest?.Latest == null)
-                {
-                    throw new InvalidOperationException("[SL]版本列表为空");
-                }else
-                {
-                    return new LatestGameCoreDownloadInfo
-                    {
-                        Release = versionsManifest.Latest.Release,
-                        Snapshot = versionsManifest.Latest.Snapshot
-                    };
-                }
-            }
-            catch (JsonException je)
-            {
-                throw new Exception("[SL]版本列表解析失败：" + je.Message, je);
-            }
-            catch (HttpRequestException hre)
-            {
-                throw new Exception("[SL]下载版本列表失败：" + hre.Message, hre);
-            }
-            catch (Exception e)
-            {
-                throw new Exception("[SL]获取版本列表时发生未知错误：" + e.Message, e);
-            }
+            GameId = gameId;
         }
     }
 }
---
+++ Utilities/InstallUtil.cs
@@ -0,0 +1,101 @@
+using System.Text.Json;
+using StarLight_Core.Models.Installer;
+
+namespace StarLight_Core.Utilities
+{
+    public class InstallUtil
+    {
+        // 获取指定版本
+        public static async Task<GameCoreDownloadInfo> GetGameCoreAsync(string id)
+        {
+            var gameCoresInfo = await GetGameCoresAsync().ConfigureAwait(false);
+            var gameCoreInfo = gameCoresInfo.FirstOrDefault(x => x.Id == id);
+            
+            if (gameCoreInfo == null)
+            {
+                throw new InvalidOperationException("[SL]版本不存在");
+            }
+            
+            return gameCoreInfo;
+        }
+        
+        // 获取版本列表
+        public static async Task<IEnumerable<GameCoreDownloadInfo>> GetGameCoresAsync()
+        {
+            try
+            {
+                var json = await HttpUtil.GetJsonAsync("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json").ConfigureAwait(false);
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    throw new InvalidOperationException("[SL]版本列表为空");
+                }
+
+                var versionsManifest = JsonSerializer.Deserialize<GameCoreJsonEntity>(json);
+                if (versionsManifest?.Version == null)
+                {
+                    throw new InvalidOperationException("[SL]版本列表为空");
+                }
+
+                return versionsManifest.Version.Select(x => new GameCoreDownloadInfo
+                {
+                    Id = x.Id,
+                    Type = x.Type,
+                    Url = x.Url,
+                    Time = x.Time,
+                    ReleaseTime = x.ReleaseTime,
+                    Sha1 = x.Sha1
+                });
+            }
+            catch (JsonException je)
+            {
+                throw new Exception("[SL]版本列表解析失败：" + je.Message, je);
+            }
+            catch (HttpRequestException hre)
+            {
+                throw new Exception("[SL]下载版本列表失败：" + hre.Message, hre);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("[SL]获取版本列表时发生未知错误：" + e.Message, e);
+            }
+        }
+        
+        // 获取最新版本
+        public static async Task<LatestGameCoreDownloadInfo> GetLatestGameCoreAsync()
+        {
+            try
+            {
+                var json = await HttpUtil.GetJsonAsync("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json").ConfigureAwait(false);
+                if (string.IsNullOrWhiteSpace(json))
+                {
+                    throw new InvalidOperationException("[SL]版本列表为空");
+                }
+
+                var versionsManifest = JsonSerializer.Deserialize<GameCoreJsonEntity>(json);
+                if (versionsManifest?.Latest == null)
+                {
+                    throw new InvalidOperationException("[SL]版本列表为空");
+                }else
+                {
+                    return new LatestGameCoreDownloadInfo
+                    {
+                        Release = versionsManifest.Latest.Release,
+                        Snapshot = versionsManifest.Latest.Snapshot
+                    };
+                }
+            }
+            catch (JsonException je)
+            {
+                throw new Exception("[SL]版本列表解析失败：" + je.Message, je);
+            }
+            catch (HttpRequestException hre)
+            {
+                throw new Exception("[SL]下载版本列表失败：" + hre.Message, hre);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("[SL]获取版本列表时发生未知错误：" + e.Message, e);
+            }
+        }
+    }
+}
