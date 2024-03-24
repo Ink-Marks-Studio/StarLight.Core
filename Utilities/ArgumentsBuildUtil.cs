e39226b2a513edc3b13ad68c8477380bbf2ae599
zhi-feng2008 <z1258909489@outlook.com>
2024-03-24 20:47:35 +0800

    修个小问题
    修个小问题

--- Utilities/ArgumentsBuildUtil.cs
+++ Utilities/ArgumentsBuildUtil.cs
@@ -190,7 +190,7 @@ public class ArgumentsBuildUtil
             { "${clientid}", "${clientid}" },
             { "${auth_xuid}", "${auth_xuid}" },
             { "${user_type}", userType },
-            { "${version_type}", $"\"{coreInfo.Type}\\SL\"" }
+            { "${version_type}", $"\"{coreInfo.Type}/SL\"" }
         };
 
         string gameDirectory = GameCoreConfig.IsVersionIsolation ?
