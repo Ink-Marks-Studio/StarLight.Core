2ca61f4fc70ae9065ee20f724d6541f3a5fbf33f
zhi-feng2008 <z1258909489@outlook.com>
2024-06-06 19:40:06 +0800

    添加下载配置
    添加下载配置

---
+++ Models/Downloader/DownloadConfig.cs
@@ -0,0 +1,19 @@
+using StarLight_Core.Downloader;
+
+namespace StarLight_Core.Models.Downloader
+{
+    public class DownloadConfig
+    {
+        public DownloadConfiguration DownloadOptions { get; set; } = new DownloadConfiguration()
+        {
+            BufferBlockSize = 10240,
+            ChunkCount = 4,
+            MaxTryAgainOnFailover = 5,
+            MaximumMemoryBufferBytes = 1024 * 1024 * 50,
+            ParallelDownload = true,
+            Timeout = 1000,
+            MinimumSizeOfChunking = 1024,
+            ReserveStorageSpaceBeforeStartingDownload = true,
+        };
+    }
+}
