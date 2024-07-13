00ff8ec329cac7733e11fea7e0d3b83087c37d14
zhi-feng2008 <z1258909489@outlook.com>
2024-07-13 23:48:40 +0800

    添加模組工具
    添加模組工具

--- Utilities/ModUtil.cs
+++ Utilities/ModUtil.cs
@@ -1,7 +1,20 @@
+using StarLight_Core.Models.Utilities;
+
 namespace StarLight_Core.Utilities
 {
+    // 模组工具
     public class ModUtil
     {
-    
+        public static IEnumerable<ModInfo> GetModsInfo(string root)
+        {
+            FileUtil.IsDirectory(root, true);
+            
+            if (!FileUtil.IsAbsolutePath(root))
+            {
+                root = FileUtil.GetCurrentExecutingDirectory() + root;
+            }
+            
+            return null;
+        }
     }
 }
