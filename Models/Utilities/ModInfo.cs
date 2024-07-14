98187589f130c0cc93161182664d7c816c2ef08f
zhi-feng2008 <z1258909489@outlook.com>
2024-07-14 21:37:54 +0800

    更新模组工具及数据类
    更新模组工具及数据类

---
+++ Models/Utilities/ModInfo.cs
@@ -0,0 +1,14 @@
+namespace StarLight_Core.Models.Utilities;
+
+public class ModInfo
+{
+    public string Name { get; set; }
+    
+    public string Id { get; set; }
+    
+    public string Version { get; set; }
+    
+    public string Author { get; set; }
+    
+    public string displayName { get; set; }
+}
---
+++ Models/Utilities/ModJsonEntity.cs
@@ -0,0 +1,29 @@
+using System.Text.Json.Serialization;
+
+namespace StarLight_Core.Models.Utilities;
+
+public class ModJsonEntity
+{
+    public class ModInfo
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("modid")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
+
+        [JsonPropertyName("authorList")]
+        public List<string> Authors { get; set; }
+
+        [JsonPropertyName("displayName")]
+        public string DisplayName { get; set; }
+
+        public string Author
+        {
+            get => Authors != null ? string.Join(", ", Authors) : null;
+        }
+    }
+}
