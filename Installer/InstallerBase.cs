22bf2ba38bd159e2bde2fa13b905c2a4cdc8e67d
ZhiFeng <z1258909489@outlook.com>
2024-10-05 13:18:28 +0800

    Docs: 添加安装器基类注释
    Docs: 添加安装器基类注释

--- Installer/InstallerBase.cs
+++ Installer/InstallerBase.cs
@@ -1,13 +1,30 @@
 namespace StarLight_Core.Installer
 {
+    /// <summary>
+    /// 安装器基类
+    /// </summary>
     public class InstallerBase
     {
+        /// <summary>
+        /// 进度报告方法
+        /// </summary>
         public Action<string,int>? OnProgressChanged { get; set; }
         
+        /// <summary>
+        /// 速度报告方法
+        /// </summary>
         public Action<string>? OnSpeedChanged { get; set; }
         
+        /// <summary>
+        /// 游戏根目录
+        /// </summary>
         protected string Root { get; set; }
     
+        /// <summary>
+        /// 速度转换方法
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         protected static string CalcMemoryMensurableUnit(double bytes)
         {
             double kb = bytes / 1024;
