5401d68c7937a133b0a6dd44e57c1bda67e9e964
ZhiFeng <z1258909489@outlook.com>
2024-10-27 17:07:12 +0800

    Wip：添加自定义安装器构造
    Wip：添加自定义安装器构造

--- Installer/CustomInstaller.cs
+++ Installer/CustomInstaller.cs
@@ -1,3 +1,7 @@
+using StarLight_Core.Downloader;
+using StarLight_Core.Enum;
+using StarLight_Core.Models.Installer;
+
 namespace StarLight_Core.Installer
 {
     /// <summary>
@@ -5,6 +9,39 @@ namespace StarLight_Core.Installer
     /// </summary>
     public class CustomInstaller : InstallerBase
     {
+        private string GameVersion { get; set; }
         
+        private string? CustomId { get; set; }    
+        
+        private CancellationToken CancellationToken { get; set; }
+        
+        /// <summary>
+        /// 自定义安装器
+        /// </summary>
+        /// <param name="gameVersion">游戏版本</param>
+        /// <param name="customId">自定义名称</param>
+        /// <param name="savePath">安装路径</param>
+        /// <param name="cancellationToken">取消令牌</param>
+        public CustomInstaller(string gameVersion, string savePath, string? customId = null, CancellationToken cancellationToken = default)
+        {
+            GameVersion = gameVersion;
+            Root = savePath;
+            CustomId = customId ?? gameVersion;
+            CancellationToken = cancellationToken;
+        }
+
+        /// <summary>
+        /// 游戏自定义安装器
+        /// </summary>
+        /// <returns></returns>
+        public async Task<InstallResult> MinecraftInstall()
+        {
+            var multiThreadedDownloader = new MultiThreadedFileDownloader(x =>
+            {
+                OnSpeedChanged?.Invoke(CalcMemoryMensurableUnit(x));
+            }, CancellationToken);
+            
+            return new InstallResult(Status.Succeeded, GameVersion, CustomId);
+        }
     }
 }
