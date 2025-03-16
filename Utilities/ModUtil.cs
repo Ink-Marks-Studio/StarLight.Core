5cc91b01fbee23134030eb7e7ab577efef775cba
ZhiFeng <z1258909489@outlook.com>
2025-03-16 16:04:34 +0800

    Wip: 模组工具初步实现
    Wip: 模组工具初步实现

--- Utilities/ModUtil.cs
+++ Utilities/ModUtil.cs
@@ -1,3 +1,5 @@
+using System.IO.Compression;
+using System.Text.Json;
 using StarLight_Core.Models.Utilities;
 
 namespace StarLight_Core.Utilities;
@@ -11,6 +13,51 @@ public class ModUtil
 
         if (!FileUtil.IsAbsolutePath(root)) root = FileUtil.GetCurrentExecutingDirectory() + root;
 
-        return null;
+        var modInfos = new List<ModInfo>();
+        var files = Directory.GetFiles(root, "*.jar", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            try
+            {
+                using var archive = ZipFile.OpenRead(file);
+                
+                // 尝试读取Fabric模组信息
+                var fabricEntry = archive.GetEntry("fabric.mod.json");
+                if (fabricEntry != null)
+                {
+                    using var reader = new StreamReader(fabricEntry.Open());
+                    var jsonContent = reader.ReadToEnd();
+                    var modJson = JsonSerializer.Deserialize<ModJsonEntity.ModInfo>(jsonContent);
+                    if (modJson != null)
+                    {
+                        modInfos.Add(new ModInfo
+                        {
+                            Name = modJson.Name,
+                            Id = modJson.Id,
+                            Version = modJson.Version,
+                            Author = modJson.Author,
+                            displayName = modJson.DisplayName
+                        });
+                        continue;
+                    }
+                }
+
+                // 尝试读取Forge模组信息
+                var forgeEntry = archive.GetEntry("META-INF/mods.toml");
+                if (forgeEntry != null)
+                {
+                    // TODO: 实现Forge模组信息的解析
+                    // 由于Forge使用TOML格式，需要添加TOML解析库
+                }
+            }
+            catch
+            {
+                // 忽略无法解析的模组文件
+                continue;
+            }
+        }
+
+        return modInfos;
     }
 }
