829a5531287eb841a533fed27c0d762978408b7e
ssp47 <ssp474138610@163.com>
2025-03-23 13:31:43 +0800

    Feat: 新增可用的模组识别功能 - 实现了Fabric端全版本模组信息获取(仅初步测试) - 实现了Forge端全版本模组信息获取(仅少量测试) - 新增模组加载器枚举(Models\MinecraftMod\Enum\ModLoaderEnum.cs) Wip: 准备实现其他模组加载器功能与更多使用方式 使用方式： 使用IMinecraftMod接口提供的静态异步迭代器方法以获得最佳性能 支持单个模组获取（不推荐加载大量模组）
    代码示例：
    ```Csharp
    await foreach(IMinecraftMod  mod:IMinecraftMod.GetModsInfoFromDirectory("你的模组文件夹"))//建议不带最后的"\"或者"/" 理由：未测试
    {
        //你对每个模组的处理代码
    }
    
    ```
    
    修正：优化性能，省去await，减少cpu资源消耗

---
+++ Models/MinecraftMod/Forge/Utils/ForgeModernToml.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace StarLight_Core.Models.MinecraftMod.Forge.Utils;
+
+internal class ForgeModernToml: ForgeModLegacyJson
+{
+    public new List<string>? Depends { get; set; }
+    /*public string ModId { get; set; }
+    public string DisplayName { get; set; }
+    public string Description { get; set; }
+    public string[] Author { get; set; }
+    public string ModVersion { get; set; }
+    public string? IconLogoPath { get; set; }*/
+    
+    
+    public ForgeModernToml(IReadOnlyList<string> toml)
+    {
+        Depends = new List<string>();
+        var modInfo = new StringBuilder();
+        var isAdd = false;
+        var isDepend = false;
+        for (var i=0;i<toml.Count;i++)
+        {
+            var line = toml[i];
+            //查找到需要的部分
+            if (line.StartsWith("[[mods]]")) isAdd = true;
+            //部分结束
+            if (line.StartsWith("[[") && !line.StartsWith("[[mods]]")) isAdd = false;
+            //查找依赖
+            if (line.StartsWith("[[dependencies")) isDepend = true;
+            if (line.StartsWith("[[") && !line.StartsWith("[[dependencies")) isDepend = false;
+            //添加
+            if (isAdd)modInfo.Append(line+" ");
+            //同时查找依赖，减少时间复杂度(?
+            if (!isDepend) continue;
+            
+            if (line.Contains("modId"))
+            {
+                var match = Regex.Match(line,@"(?<=\s*=\s*[""'])(.*?)(?=[""'])");
+                Depends.Add(match.Value);
+            }
+
+            if (!line.Contains("mandatory")) continue;
+            {
+                var match = Regex.Match(line,@"(?i)(?<=\s*=\s*)(true|false)\b");
+                if (match.Value.ToLower() == "false")
+                {
+                    Depends.Remove(Regex.Match(toml[i - 1], @"(?<=\s*=\s*[""'])(.*?)(?=[""'])").Value);
+                }
+            }
+        }
+        //捕获赋值
+        foreach (Match match in Regex.Matches(modInfo.ToString(),@"(\w+)\s*=\s*(?:""([^""]*)""|'([^']*?)'|'''(.*?)''')(?:\s+|$)"))
+        {
+
+            switch (match.Groups[1].Value)
+            {
+                case "modId":
+                    ModId = match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value;
+                    break;
+                case "version":
+                    ModVersion = match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value;
+                    break;
+                case "displayName":
+                    DisplayName = match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value;
+                    break;
+                case "authors":
+                    Author = new[] { match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value };
+                    break;
+                case "description":
+                    Description = match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value;
+                    break;
+                case "logoFile":
+                    IconLogoPath = match.Groups[4].Success ? match.Groups[4].Value : match.Groups[2].Value;
+                    break;
+            }
+        }
+        
+        
+    }
+}
