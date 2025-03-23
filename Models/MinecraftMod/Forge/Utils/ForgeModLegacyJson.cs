a1c44b2037aec55a10b0688ee5213e02cdc7e9e0
ssp47 <ssp474138610@163.com>
2025-03-23 13:31:43 +0800

    Feat: 新增可用的模组识别功能 - 实现了Fabric端全版本模组信息获取(仅初步测试) - 实现了Forge端全版本模组信息获取(仅少量测试) - 新增模组加载器枚举(Models\MinecraftMod\Enum\ModLoaderEnum.cs) Wip: 准备实现其他模组加载器功能与更多使用方式 使用方式： 使用IMinecraftMod接口提供的静态异步迭代器方法以获得最佳性能 支持单个模组获取（不推荐加载大量模组）
    代码示例：
    ```Csharp
    await foreach(IMinecraftMod  mod:IMinecraftMod.GetModsInfoFromDirectory("你的模组文件夹"))//建议不带最后的"\"或者"/" 理由：未测试
    {
        //你对每个模组的处理代码
    }
    
    ```

---
+++ Models/MinecraftMod/Forge/Utils/ForgeModLegacyJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace StarLight_Core.Models.MinecraftMod.Forge.Utils;
+
+internal class ForgeModLegacyJson
+{
+    [JsonPropertyName("dependencies")] public string[]? Depends { get; set; }
+    [JsonPropertyName("modid")] public string ModId { get; set; }
+    [JsonPropertyName("name")] public string DisplayName { get; set; }
+    [JsonPropertyName("description")] public string Description { get; set; }
+    [JsonPropertyName("authorList")] public string[] Author { get; set; }
+    [JsonPropertyName("version")] public string ModVersion { get; set; }
+    [JsonPropertyName("logoFile")] public string? IconLogoPath { get; set; }
+}
